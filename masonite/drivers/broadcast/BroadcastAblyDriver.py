6460ac855d3671c54745f831ac0a891a28baabb8
Abhijit Mamarde <abhijit.mamarde@gmail.com>
2020-03-03 12:51:47 +0530

    fixed issue for ably broadcast driver: 'BroadcastConfig key was not found in the container'

--- masonite/drivers/broadcast/BroadcastAblyDriver.py
+++ masonite/drivers/broadcast/BroadcastAblyDriver.py
@@ -4,6 +4,7 @@ from masonite.contracts import BroadcastContract
 from masonite.drivers import BaseDriver
 from masonite.exceptions import DriverLibraryNotFound
 from masonite.app import App
+from masonite.helpers import config
 
 
 class BroadcastAblyDriver(BroadcastContract, BaseDriver):
@@ -15,7 +16,6 @@ class BroadcastAblyDriver(BroadcastContract, BaseDriver):
         Arguments:
             BroadcastConfig {config.broadcast} -- Broadcast configuration setting.
         """
-        self.config = app.make('BroadcastConfig')
         self.ssl_message = True
 
     def ssl(self, boolean):
@@ -52,8 +52,9 @@ class BroadcastAblyDriver(BroadcastContract, BaseDriver):
             raise DriverLibraryNotFound(
                 'Could not find the "ably" library. Please pip install this library running "pip install ably"')
 
+        configuration = config('broadcast.drivers.ably')
         client = AblyRest('{0}'.format(
-            self.config.DRIVERS['ably']['secret']
+            configuration['secret']
         ))
 
         if isinstance(channels, list):
